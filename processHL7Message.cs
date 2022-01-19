55d1b1218e86f780eeeaf9e0ec2434557f9de6f5
Joseph Roberts <joseph.roberts@careaius.onmicrosoft.com>
2022-01-19 09:39:54 -0500

    removing unused using statement

--- processHL7Message.cs
+++ processHL7Message.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Threading;
